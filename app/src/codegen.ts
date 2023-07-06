6bf5b6873d417387c2f329f419d0fb56eaa4c32b
Mariusz Pilarek <mariusz@eiger.co>
2023-07-06 13:16:58 +0200

    Code reformat.

--- app/src/codegen.ts
+++ app/src/codegen.ts
@@ -1,49 +1,50 @@
 export class CodeGen {
-    public generateHeader(author: String, license: String) {
-        let header = '';
-        if (author !== '') {
-            header = `//author: ${author}\n`;
-        }
-        if (license !== '') {
-            header += `//license: ${license}\n`;
-        }
-        header += '\n#![no_std]\n';
-        header += '\nuse soroban_sdk::{contractimpl, Env};\n';
+  public generateHeader (author: string, license: string): string {
+    let header = ''
+    if (author !== '') {
+      header = `//author: ${author}\n`
+    }
+    if (license !== '') {
+      header += `//license: ${license}\n`
+    }
+    header += '\n#![no_std]\n'
+    header += '\nuse soroban_sdk::{contractimpl, Env};\n'
 
-        return header;
+    return header
+  }
+
+  public generateImports (imports: string[]): string {
+    let importsStr = ''
+    for (const imp of imports) {
+      importsStr += `#[import]\nmod ${imp} {}\n\n`
+    }
+    return importsStr
+  }
+
+  public generateContract (name: string, params: string[]): string {
+    let contractCode = ''
+    contractCode = `pub struct ${name};\n`
+    contractCode += '\n#[contractimpl]\n'
+    contractCode += `impl ${name} {`
+    contractCode += `
+  pub fn init(Env env) {
+    let contract_id = env.register_contract(None, Lottery::WASM);
+    let client = Lottery::Client::new(&env, contract_id);`
+
+    contractCode += `
+    client.init(`
+    for (const param of params) {
+      contractCode += `${param}, `
+    }
+    // remove last comma
+    if (params.length !== 0) {
+      contractCode = contractCode.slice(0, -2)
     }
 
-    public generateImports(imports: Array<String>) {
-        let importsStr = '';
-        for (let imp of imports) {
-            importsStr += `#[import]\nmod ${imp} {}\n\n`;
-        }
-        return importsStr;
-    }
+    contractCode += `);
+  }\n`
+    contractCode += '}\n\n'
 
-    public generateContract(name: String, params: Array<String>) {
-        let contractCode = '';
-        contractCode = `pub struct ${name};\n`;
-        contractCode += '\n#[contractimpl]\n';
-        contractCode += `impl ${name} {`;
-        contractCode += `
-    pub fn init(Env env) {
-        let contract_id = env.register_contract(None, Lottery::WASM);
-        let client = Lottery::Client::new(&env, contract_id);`
-
-        contractCode += `
-        client.init(`;
-        for (let param of params) {
-            contractCode += `${param}, `;
-        }
-        //remove last comma
-        if (params.length !== 0)
-            contractCode = contractCode.slice(0, -2);
-
-        contractCode += `);
-    }\n`;
-        contractCode += '}\n\n';
-
-        return contractCode;
-    }
+    return contractCode
+  }
 }
--- app/src/editor.ts
+++ app/src/editor.ts
@@ -1,15 +1,16 @@
-import hljs from "highlight.js";
-import 'highlight.js/styles/github-dark.css';
-import { CodeGen } from "./codegen";
-import "./style.css";
+import hljs from 'highlight.js'
+import 'highlight.js/styles/github-dark.css'
+import { CodeGen } from './codegen'
+import './style.css'
 
+const codeGen = new CodeGen()
+let contractCode = codeGen.generateHeader('author', 'MIT')
+contractCode += '\n' + codeGen.generateImports(['Lottery'])
+contractCode += '\n' + codeGen.generateContract('MyContract', ['true', '100', '10', '1'])
 
-let codeGen = new CodeGen();
-let contractCode = codeGen.generateHeader('author', 'MIT');
-contractCode += '\n' + codeGen.generateImports(['Lottery']);
-contractCode += '\n' + codeGen.generateContract('MyContract', ['true', '100', '10', '1']);
+const html = hljs.highlight(contractCode, { language: 'rust' }).value
 
-let html = hljs.highlight(contractCode, {language: 'rust'}).value
-
-document.querySelector<HTMLDivElement>("#editor")!.innerHTML = 
-  '<pre><code class="language-rust">' + html + '</code></pre>';
+const editor = document.querySelector<HTMLDivElement>('#editor')
+if (editor != null) {
+  editor.innerHTML = '<pre><code class="language-rust">' + html + '</code></pre>'
+}
