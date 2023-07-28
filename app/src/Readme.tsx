1321f33e676db349696dbda7fc76ca60b6c6ce81
eloylp <eloylp@eloylp.dev>
2023-07-28 13:57:11 +0200

    FIx linter issues

--- app/src/Readme.tsx
+++ app/src/Readme.tsx
@@ -7,21 +7,22 @@ export function Readme({ currentPage }: any) {
       <Navbar currentPage="#/" />
       <h2>What is it?</h2>
       <p>
-        Nebula is a tool for easing development of Soroban smart contracts being highly cohesive
-        with the existing ecosystem.
+        Nebula is a tool for easing development of Soroban smart contracts being
+        highly cohesive with the existing ecosystem.
       </p>
       <h2>Development</h2>
       <p>
         Nebula aims to provide the following functionality:
         <ul>
           <li>
-            A contract wizard UI, that will help developers to graphically compose smart contracts from templates
-            which are ready for production use and audited.
+            A contract wizard UI, that will help developers to graphically
+            compose smart contracts from templates which are ready for
+            production use and audited.
           </li>
           <li>
-            A highly cohesive Soroban Rust contract library, that will contain the contracts that
-            the wizard will use. Developers can also make use of this library interfaces and implementations
-            directly.
+            A highly cohesive Soroban Rust contract library, that will contain
+            the contracts that the wizard will use. Developers can also make use
+            of this library interfaces and implementations directly.
           </li>
           <li>
             A contract management system via{' '}
@@ -30,12 +31,14 @@ export function Readme({ currentPage }: any) {
             </a>
           </li>
         </ul>
-
-        For an updated status of current developments visit our <a href="https://github.com/eigerco/nebula">github repository</a> issues and discussions.
+        For an updated status of current developments visit our{' '}
+        <a href="https://github.com/eigerco/nebula">github repository</a> issues
+        and discussions.
       </p>
       <h2>Common interfaces </h2>
       <p>
-        Nebula aims to provide interfaces for commonly used contacts. This is not an exhaustive list.
+        Nebula aims to provide interfaces for commonly used contacts. This is
+        not an exhaustive list.
         <ul>
           <li>Math</li>
           <li>Voting</li>
