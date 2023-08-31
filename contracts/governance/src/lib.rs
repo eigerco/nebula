cf5e7da991f972a52809241161dc8824b3055302
eloylp <eloylp@eloylp.dev>
2023-08-31 10:56:19 +0200

    [GC] Add the `join()` function to the contract
    Participants can now join the DAO.
    - It checks for current balances before doing anything else.
    - It transfer the initial amount to the contract address and also internaly keeps the balance.
    - It emits the proper contract event "participant_joined".
    - It checks authorization.
    - It checks for negative amounts.

--- contracts/governance/src/lib.rs
+++ contracts/governance/src/lib.rs
@@ -7,7 +7,8 @@
 #![no_std]
 
 use soroban_sdk::{
-    contract, contracterror, contractimpl, contracttype, panic_with_error, Address, Env,
+    contract, contracterror, contractimpl, contracttype, panic_with_error, token, Address, Env,
+    Map, Symbol,
 };
 
 /// Datakey holds all possible storage keys this
@@ -18,6 +19,7 @@ enum DataKey {
     Initialized = 1,
     Curator = 2,
     Token = 3,
+    Participants = 4,
 }
 
 /// All the expected errors this contract expects.
@@ -29,6 +31,10 @@ enum DataKey {
 enum Error {
     /// The contract should be only initialized once.
     AlreadyInitialized = 1,
+    // Must have funds for the operation.
+    InsufficientFunds = 2,
+    // Amounts cannot be negative in some operations.
+    UnderZeroAmount = 3,
 }
 
 #[contract]
@@ -42,9 +48,70 @@ impl GovernanceContract {
         if storage.has(&DataKey::Initialized) {
             panic_with_error!(&env, Error::AlreadyInitialized)
         }
+
         storage.set(&DataKey::Initialized, &());
         storage.set(&DataKey::Curator, &curator);
         storage.set(&DataKey::Token, &token);
+        storage.set(
+            &DataKey::Participants,
+            &Map::<Address, Participant>::new(&env),
+        );
+    }
+
+    pub fn join(env: Env, participant_addr: Address, initial_stake: i128) {
+        participant_addr.require_auth();
+
+        if initial_stake <= 0 {
+            panic_with_error!(&env, Error::UnderZeroAmount);
+        }
+
+        let storage = env.storage().persistent();
+        let token_addr = storage.get::<_, Address>(&DataKey::Token).unwrap();
+        let token_client = token::Client::new(&env, &token_addr);
+        let balance = token_client.balance(&participant_addr);
+
+        if balance < initial_stake {
+            panic_with_error!(&env, Error::InsufficientFunds)
+        }
+
+        token_client.transfer(
+            &participant_addr,
+            &env.current_contract_address(),
+            &initial_stake,
+        );
+
+        let mut participants = storage
+            .get::<_, Map<Address, Participant>>(&DataKey::Participants)
+            .unwrap();
+
+        participants.set(
+            participant_addr.clone(),
+            Participant::new(participant_addr.clone(), initial_stake),
+        );
+
+        storage.set(&DataKey::Participants, &participants);
+
+        env.events().publish(
+            (Symbol::new(&env, "participant_joined"), participant_addr),
+            initial_stake,
+        );
+    }
+}
+
+#[contracttype]
+struct Participant {
+    address: Address,
+    whitelisted: bool,
+    current_balance: i128,
+}
+
+impl Participant {
+    pub fn new(address: Address, initial_stake: i128) -> Self {
+        Participant {
+            address,
+            whitelisted: false,
+            current_balance: initial_stake,
+        }
     }
 }
 
--- contracts/governance/src/test.rs
+++ contracts/governance/src/test.rs
@@ -4,7 +4,10 @@ extern crate std;
 
 use super::{GovernanceContract, GovernanceContractClient};
 
-use soroban_sdk::{testutils::Address as _, token, Address, Env};
+use soroban_sdk::{
+    testutils::{Address as _, AuthorizedFunction, AuthorizedInvocation, Events},
+    token, vec, Address, Env, IntoVal, Symbol, Val, Vec,
+};
 
 #[test]
 #[should_panic(expected = "Error(Contract, #1)")]
@@ -23,4 +26,150 @@ fn cannot_be_initialized_twice() {
 
 fn create_token_contract<'a>(e: &Env, admin: &Address) -> token::AdminClient<'a> {
     token::AdminClient::new(e, &e.register_stellar_asset_contract(admin.clone()))
+}
+
+#[test]
+fn participant_can_join() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(Some(&Address::random(&env)), GovernanceContract);
+    let client = GovernanceContractClient::new(&env, &contract_id);
+
+    let token_admin = Address::random(&env);
+    let token_addr = &env.register_stellar_asset_contract(token_admin.clone());
+    let token_admin_client = token::AdminClient::new(&env, token_addr);
+    let token_client = token::Client::new(&env, token_addr);
+
+    let participant_addr = Address::random(&env);
+    // Add funds to client address (as participant)
+    token_admin_client.mint(&participant_addr, &1000);
+
+    // Init contract
+    let curator = Address::random(&env);
+    client.init(&curator, &token_admin_client.address);
+
+    // Join the participant (in this case same as client)
+    let initial_stake = 200;
+    client.join(&participant_addr, &initial_stake);
+
+    // Ensure we check participant is who says.
+    assert_auth(
+        &env.auths(),
+        0,
+        participant_addr.clone(),
+        client.address.clone(),
+        Symbol::new(&env, "join"),
+        (participant_addr.clone(), initial_stake).into_val(&env),
+    );
+
+    // After joining, the contract should have received participant funds.
+    // The participant should have less funds.
+    assert_eq!(200, token_client.balance(&contract_id));
+    assert_eq!(800, token_client.balance(&participant_addr));
+
+    // A proper joining event should be published.
+    let last_event = env.events().all().last().unwrap();
+    assert_eq!(
+        vec![&env, last_event],
+        vec![
+            &env,
+            (
+                contract_id,
+                (Symbol::new(&env, "participant_joined"), participant_addr).into_val(&env),
+                200i128.into_val(&env)
+            ),
+        ]
+    )
+}
+
+fn assert_auth(
+    auths: &[(Address, AuthorizedInvocation)],
+    idx: usize,
+    call_addr: Address,
+    auth_addr: Address,
+    func: Symbol,
+    args: Vec<Val>,
+) {
+    let auth = auths.get(idx).unwrap();
+    assert_eq!(auth.0, call_addr);
+    assert_eq!(
+        auth.1.function,
+        AuthorizedFunction::Contract((auth_addr, func, args))
+    );
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #2)")]
+fn participant_cant_join_without_enough_funds() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(Some(&Address::random(&env)), GovernanceContract);
+    let client = GovernanceContractClient::new(&env, &contract_id);
+
+    let token_admin = Address::random(&env);
+    let token_addr = &env.register_stellar_asset_contract(token_admin.clone());
+    let token_admin_client = token::AdminClient::new(&env, token_addr);
+
+    let participant_addr = Address::random(&env);
+    // Add funds to client address (as participant)
+    token_admin_client.mint(&participant_addr, &199);
+
+    // Init contract
+    let curator = Address::random(&env);
+    client.init(&curator, &token_admin_client.address);
+
+    // Join the participant (in this case same as client). Should not have enough funds. We expect panic.
+    client.join(&participant_addr, &200);
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #3)")]
+fn participant_cant_join_with_negative_stake() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(Some(&Address::random(&env)), GovernanceContract);
+    let client = GovernanceContractClient::new(&env, &contract_id);
+
+    let token_admin = Address::random(&env);
+    let token_addr = &env.register_stellar_asset_contract(token_admin.clone());
+    let token_admin_client = token::AdminClient::new(&env, token_addr);
+
+    let participant_addr = Address::random(&env);
+    // Add funds to client address (as participant)
+    token_admin_client.mint(&participant_addr, &199);
+
+    // Init contract
+    let curator = Address::random(&env);
+    client.init(&curator, &token_admin_client.address);
+
+    // Join the participant (in this case same as client). Should not have enough funds. We expect panic.
+    client.join(&participant_addr, &-1);
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #3)")]
+fn participant_cant_join_with_zero_stake() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(Some(&Address::random(&env)), GovernanceContract);
+    let client = GovernanceContractClient::new(&env, &contract_id);
+
+    let token_admin = Address::random(&env);
+    let token_addr = &env.register_stellar_asset_contract(token_admin.clone());
+    let token_admin_client = token::AdminClient::new(&env, token_addr);
+
+    let participant_addr = Address::random(&env);
+    // Add funds to client address (as participant)
+    token_admin_client.mint(&participant_addr, &199);
+
+    // Init contract
+    let curator = Address::random(&env);
+    client.init(&curator, &token_admin_client.address);
+
+    // Join the participant (in this case same as client). Should not have enough funds. We expect panic.
+    client.join(&participant_addr, &0);
 }
