4f8fc8e916f2ba4086f3d67b3f4786b3028af013
Mariusz Pilarek <mariusz@eiger.co>
2023-09-06 14:34:02 +0200

    Added script that runs all integration tests, removed unneeded rust test files.

---
+++ integration_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd contracts
+exit_code=0
+for d in *; do
+  cd $d  
+  echo "------------------------------------"
+  echo "Running test for $d contract"
+  echo "------------------------------------"
+  cargo make run_test
+  if [ "$?" != 0 ]; then
+    let exit_code=1
+  fi
+  cd ../
+done
+exit $exit_code
