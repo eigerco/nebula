87d4cfda33a4e1b629fb493ebf33bbb20feb9d09
Mariusz Pilarek <mariusz@eiger.co>
2023-09-07 14:11:30 +0200

    Another one

--- integration_tests.sh
+++ integration_tests.sh
@@ -2,6 +2,13 @@
 
 cd contracts
 exit_code=0
+
+{ ERR=$( { echo "sth" 1>&3 ; } 2>&1); } 3>&1
+if [ ! -z "$ERR" ]; then
+  echo "$ERR"
+  exit 1
+fi
+
 for d in *; do
   cd $d  
   echo "------------------------------------"
